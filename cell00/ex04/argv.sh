2ba977ad7127028ceda734ac9c08699a631e8e1c
66070010Ninja <141380362+66070010Ninja@users.noreply.github.com>
2025-02-04 21:10:37 +0700

    Update argv.sh

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Check how many arguments are provided and display them (max 3)
 if [ $# -eq 0 ]; then
-    echo "No arguments supplied"
+    echo "No arguments provided."
 else
-    for arg in "$@"; do
-        echo "$arg"
+    for i in {1..3}; do
+        if [ -n "${!i}" ]; then
+            echo "${!i}"
+        fi
     done
 fi
-
