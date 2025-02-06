0b5390417f6ef540b9355b2c3bcabf79c905ced8
66070010Ninja <66070010@kmitl.ac.th>
2025-02-06 19:05:48 +0700

    ex03

--- cell03/ex03/todo.js
+++ cell03/ex03/todo.js
@@ -1,21 +1,59 @@
-function addTask() {
-    let task = prompt("Enter a new TO DO:");
-    if (task) {
-        create(task);
+document.addEventListener("DOMContentLoaded", function () {
+    loadTasks();
+});
+
+function newTask() {
+    const taskName = prompt("Enter a new task name:");
+    if (taskName !== null && taskName.trim() !== "") {
+        addTask(taskName);
+        saveTasksToCookie();
     }
 }
 
-function create(name) {
-    let taskList = document.getElementById("ft_lilst");
-    let task = document.createElement('div');
-    task.className = "todo";
-    task.textContent = name;
-    task.onclick = function() {
-        if (confirm("Do you really want to remove this TO DO?")) {
-            task.remove();
+function addTask(name) {
+    const taskList = document.getElementById("ft_list");
+    const taskContainer = document.createElement("ul");
+    const taskData = document.createElement("li");
+
+    taskData.textContent = name;
+    taskContainer.classList.add("todo-item");
+    taskContainer.addEventListener("click", function () {
+        const isDelete = confirm("Do you want to delete this task?");
+        if(isDelete) {
+            deleteTask(taskContainer);
+            saveTasksToCookie();
+        }
+    });
+    taskContainer.appendChild(taskData);
+    taskList.prepend(taskContainer);
+}
+
+function deleteTask(taskId) {
+    taskId.remove();
+}
+
+function saveTasksToCookie() {
+    const taskList = document.getElementById("ft_list");
+    const tasks = [];
+    
+    taskList.childNodes.forEach(function (taskItem) {
+        if (taskItem.nodeName === "UL") {
+            tasks.push(taskItem.firstChild.textContent);
+        }
+    });
+
+    document.cookie = "tasks=" + JSON.stringify(tasks);
+}
+
+function loadTasks() {
+    const cookies = document.cookie.replace("tasks=[","").replace("]","").replace(/"/g, '').split(",");
+    console.log(cookies);
+    
+    if(cookies[0] != "") {
+        for(let i = cookies.length-1 ; i >= 0  ; i--) {
+            addTask(cookies[i])
+            console.log(cookies[i]);
+        
         }
     }
-    let list = document.getElementById("ft_list");
-    list.insertBefore(task, list.firstChild);
 }
-
